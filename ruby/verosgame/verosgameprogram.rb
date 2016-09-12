6d0206d5544057170d7c7ff66c14423939133e45
Veronica Morales <veronica.morales88@gmail.com>
2016-09-11 23:15:20 -0400

    more changes

--- ruby/verosgame/verosgameprogram.rb
+++ ruby/verosgame/verosgameprogram.rb
@@ -1,43 +1,43 @@
-# game class
+# # game class
 
-class CupGame
-  attr_reader :guess_count
-  attr_reader :is_over
+# class CupGame
+#   attr_reader :guess_count
+#   attr_reader :is_over
 	
-  def initialize
-    @cups = ["ball", "empty", "empty"]
-    @guess_count = 0
-    @is_over = false
-  end
+#   def initialize
+#     @cups = ["ball", "empty", "empty"]
+#     @guess_count = 0
+#     @is_over = false
+#   end
   
-  def shuffle
-  	@cups.shuffle!
-  end
+#   def shuffle
+#   	@cups.shuffle!
+#   end
   
-  def check_cup(index)
-  	@guess_count += 1
-  	if @cups[index] == "ball"
-  		@is_over = true
-  	else
-  		false
-  	end
-  end
-end
+#   def check_cup(index)
+#   	@guess_count += 1
+#   	if @cups[index] == "ball"
+#   		@is_over = true
+#   	else
+#   		false
+#   	end
+#   end
+# end
 
-# user interface
+# # user interface
 
-puts "Welcome to the Cup Game!"
-game = CupGame.new
+# puts "Welcome to the Cup Game!"
+# game = CupGame.new
 
-puts "Shuffling cups ..."
-game.shuffle
+# puts "Shuffling cups ..."
+# game.shuffle
 
-while !game.is_over
-  puts "Which cup has the ball? Enter a guess of 1, 2, or 3:"
-  guess = gets.chomp.to_i
-  if !game.check_cup(guess - 1)
-  	puts "Nope! Try again."
-  end
-end
+# while !game.is_over
+#   puts "Which cup has the ball? Enter a guess of 1, 2, or 3:"
+#   guess = gets.chomp.to_i
+#   if !game.check_cup(guess - 1)
+#   	puts "Nope! Try again."
+#   end
+# end
 
-puts "You won in #{game.guess_count} guesses!"
+# puts "You won in #{game.guess_count} guesses!"
